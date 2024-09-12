ed0de278414667205be0c1294f55261918582c00
SID <sid@Tajammal-Hussains-mac.local>
2024-09-12 21:17:35 +0500

    Initial project upload

---
+++ screenSharing/screenSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenSharingApp.swift
+//  screenSharing
+//
+//  Created by SID on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct screenSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
