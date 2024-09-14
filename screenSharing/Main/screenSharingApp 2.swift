7ba0b611e7735a40b07d66effd99fd6c6f42c7a0
SID <sid@Tajammal-Hussains-mac.local>
2024-09-15 00:42:49 +0500

    ..

---
+++ screenSharing/Main/screenSharingApp 2.swift
@@ -0,0 +1,14 @@
+
+
+import SwiftUI
+
+@main
+struct ScreensharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //PreJoinScreen()
+            MeetingOptionsView()
+            //VideoClientSetUp()
+        }
+    }
+}
