7e0d5d31433e5798b6a08b80ba03687ac8976460
SID <managebydev@gmail.com>
2024-09-25 18:23:44 +0500

    creeted New project

---
+++ screenSharingApp/screenSharingApp/screenSharingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenSharingAppApp.swift
+//  screenSharingApp
+//
+//  Created by SID on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct screenSharingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ screenSharingApp/screenSharingAppTests/screenSharingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  screenSharingAppTests.swift
+//  screenSharingAppTests
+//
+//  Created by SID on 25/09/2024.
+//
+
+import Testing
+@testable import screenSharingApp
+
+struct screenSharingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
