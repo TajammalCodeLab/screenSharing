f308e5ccdf191581fbd2d0406cb7be1cd7a01a68
SID <managebydev@gmail.com>
2024-09-25 20:22:01 +0500

    Added files from previous project

---
+++ screenSharingApp/screenSharingApp/AppComponents/UIs/CustomUIFactory.swift
@@ -0,0 +1,24 @@
+//
+//  CustomUIFactory.swift
+//  screenSharingApp
+//
+//  Created by SID on 25/09/2024.
+//
+
+import SwiftUI
+import StreamVideo
+import StreamVideoSwiftUI
+
+class CustomViewFactory: ViewFactory {
+    // 1. Custom Call Controls
+    public func makeCallControlsView(viewModel: CallViewModel) -> some View {
+        AroundMeetingControlsView(viewModel: viewModel)
+            .padding(.bottom)
+    }
+    
+    // 2. Custom CallTopView
+    public func makeCallTopView(viewModel: CallViewModel) -> some View {
+        AroundMeetingTopView(viewModel: viewModel, participants: [])
+            .padding(.bottom, 42)
+    }
+}
